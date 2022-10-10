1d81414dadd4916cd315789f74a7009565c845a8
SpecialDude <warithadetayo.awa@gmail.com>
2022-10-10 04:38:35 +0100

    Bubble sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts array using Bubble sort Algorith
+ *
+ * @array: Array to be sorted
+ * @size: Number of element to be sorted
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int sorted, temp;
+
+	j = 0;
+	sorted = 0;
+	while (!sorted)
+	{
+		sorted = 1;
+		j++;
+
+		for (i = 0; i < size - j; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				sorted = 0;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ tests/0-bubble_sort_main.c
@@ -0,0 +1,17 @@
+#include "../sort.h"
+
+int main(void)
+{
+	int array[] = {19, 48, 99, 71, 13, 53, 96, 73, 86, 7};
+    int array2[] = {19, 48, 99, 71, 13, 53, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
+
+	print_array(array, n);
+	printf("\n");
+	bubble_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	printf("\n");
+
+	return (0);
+}
